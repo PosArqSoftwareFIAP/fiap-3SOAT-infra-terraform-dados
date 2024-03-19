cffff97c7005210945ff332dea57b011d28e4c1f
WilliamMp <38511966+WilliamMp@users.noreply.github.com>
2024-03-18 23:48:36 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,13 @@
-provider "digitalocean" {
-  token = local.do_token
+
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = ">= 2.0.0" # specify your required version
+    }
+  }
 }
 
+provider "digitalocean" {
+  token = var.do_token
+}
